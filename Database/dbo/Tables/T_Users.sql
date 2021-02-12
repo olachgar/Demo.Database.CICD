99c33953776a77bd09411ae7fd98fba8a5a3128e
Omar LACHGAR <omar.lachgar.softeam@axa.fr>
2021-02-12 11:07:06 +0100

    feat : add 1st database object (users table)

---
+++ Database/dbo/Tables/T_Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[T_Users] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (50) NULL,
+    [LastName]    NVARCHAR (50) NULL,
+    [DateOfBirth] DATE          NULL,
+    CONSTRAINT [PK_T_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
